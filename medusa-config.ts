d1bc5ffe95b76f46076856fed83436192a684ef5
Nathan-white-cyber <iamm786@gmail.com>
2026-06-06 19:39:45 -0700

    Hardcode fallback connection strings

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,10 +1,8 @@
-import { loadEnv, defineConfig } from '@medusajs/utils'
-
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+import { defineConfig } from '@medusajs/utils'
 
 module.exports = defineConfig({
   projectConfig: {
-    databaseUrl: process.env.DATABASE_URL,
+    databaseUrl: process.env.DATABASE_URL || "postgresql://postgres:jRLHPKDvoZByxAYIvFiCqKemGYBranln@postgres.railway.internal:5432/railway?connection_limit=3&pool_timeout=60",
     databaseDriverOptions: {
       pool: {
         min: 1,
@@ -13,13 +11,13 @@ module.exports = defineConfig({
         idleTimeoutMillis: 30000,
       },
     },
-    redisUrl: process.env.REDIS_URL,
+    redisUrl: process.env.REDIS_URL || "redis://default:OpcyFXaRzKwTYwoTqpWzJBmHyPljcfHP@redis.railway.internal:6379",
     http: {
-      storeCors: process.env.STORE_CORS || 'https://omg-retro.vercel.app',
-      adminCors: process.env.ADMIN_CORS || 'https://omg-retro.vercel.app',
-      authCors: process.env.STORE_CORS || 'https://omg-retro.vercel.app',
-      jwtSecret: process.env.JWT_SECRET || 'omgretro-jwt-secret-2026-secure',
-      cookieSecret: process.env.COOKIE_SECRET || 'omgretro-cookie-secret-2026-secure',
+      storeCors: process.env.STORE_CORS || "https://omg-retro.vercel.app",
+      adminCors: process.env.ADMIN_CORS || "https://omg-retro.vercel.app",
+      authCors: process.env.STORE_CORS || "https://omg-retro.vercel.app",
+      jwtSecret: process.env.JWT_SECRET || "omgretro-jwt-secret-2026-secure",
+      cookieSecret: process.env.COOKIE_SECRET || "omgretro-cookie-secret-2026-secure",
     },
   },
   admin: {
